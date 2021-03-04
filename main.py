1059ea89080c8ab0b9086a4800a9b229b68872ad
Aldric Gaudinot <gaudinot@gmail.com>
2021-03-04 18:39:25 +0100

    Add logging + simplify launch + add trigger

--- main.py
+++ main.py
@@ -1,44 +1,93 @@
+import json
+import logging
 import os
+import subprocess
 from concurrent.futures import TimeoutError
+
+import google.cloud.logging
 from google.cloud import pubsub_v1
 
-def receive_messages_with_custom_attributes(project_id, subscription_id, timeout=None):
-    """Receives messages from a pull subscription."""
-    # [START pubsub_subscriber_async_pull_custom_attributes]
 
-    subscriber = pubsub_v1.SubscriberClient()
-    subscription_path = subscriber.subscription_path(project_id, subscription_id)
+class HugoRunner:
+    def __init__(self, project_id, subscription_id, notification_topic, bucket_in, bucket_out, publish_script, timeout=None):
+        self.project_id = project_id
+        self.subscription_id = subscription_id
+        self.notification_topic = notification_topic
+        self.bucket_in  = bucket_in
+        self.bucket_out = bucket_out
+        self.publish_script = publish_script
 
-    def callback(message):
-        print(f"Received {message.data}.")
-        if message.attributes:
-            print("Attributes:")
-            for key in message.attributes:
-                value = message.attributes.get(key)
-                print(f"{key}: {value}")
+        self.timeout = timeout
+        
+        self.publisher = pubsub_v1.PublisherClient()
+        self.notification_topic_path = self.publisher.topic_path(project_id, notification_topic)
+
+    
+    def notify(self, payload, update):
+        logging.info(f"Notify :{payload}, {update}.")
+        
+        _payload = {**payload, **update}
+        data = json.dumps(_payload).encode("utf-8")
+        notification = self.publisher.publish(self.notification_topic_path, data)
+        logging.info(notification.result())
+
+    def run_build(self, payload):
+        buildId = payload['buildId']
+        environment = payload['environment']
+        website = payload['website']
+        theme = payload['theme']
+        buildId = payload['buildId']
+        output = subprocess.run(["bash", self.publish_script, environment, website, self.bucket_in, self.bucket_out, buildId, theme], capture_output=True, text=True)
+        return { 'returncode': output.returncode, 'stderr' : output.stderr , 'stdout': output.stdout, 'args': output.args } 
+
+    def callback(self, message):
+        logging.info(f"Received {message.data}.")
+
+        payload = json.loads(message.data)
         message.ack()
 
-    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-    print(f"Listening for messages on {subscription_path}..\n")
+        self.notify(payload, { 'status' : 'running'})
+        result = self.run_build(payload)
+        self.notify(payload, {'output'  : result, 'status' : 'succeded' if result.returncode == 0 else 'failed' })
+        
 
-    # Wrap subscriber in a 'with' block to automatically call close() when done.
-    with subscriber:
-        try:
-            # When `timeout` is not set, result() will block indefinitely,
-            # unless an exception is encountered first.
-            streaming_pull_future.result(timeout=timeout)
-        except TimeoutError:
-            streaming_pull_future.cancel()
-    # [END pubsub_subscriber_async_pull_custom_attributes]
+    def receive_publish_request(self):
+        """Receives messages from a pull subscription."""
+        # [START pubsub_subscriber_async_pull_custom_attributes]
+        subscriber = pubsub_v1.SubscriberClient()
+        subscription_path = subscriber.subscription_path(self.project_id, self.subscription_id)
+        streaming_pull_future = subscriber.subscribe(subscription_path, callback=self.callback)
+        logging.info(f"Listening for messages on {subscription_path}..\n")
+        # Wrap subscriber in a 'with' block to automatically call close() when done.
+        with subscriber:
+            try:
+                # When `timeout` is not set, result() will block indefinitely,
+                # unless an exception is encountered first.
+                streaming_pull_future.result(timeout=self.timeout)
+            except TimeoutError:
+                streaming_pull_future.cancel()
+        # [END pubsub_subscriber_async_pull_custom_attributes]
+
 
 if __name__ == "__main__":
-    
+
+    client = google.cloud.logging.Client()
+    client.get_default_handler()
+    client.setup_logging()
+
     project_id = os.environ.get('PUBSUB_PROJECT')
     subscription_id = os.environ.get('PUBSUB_PUBLISH_SUBSCRIPTION')
     publish_topic_name = os.environ.get('PUBSUB_PUBLISH_TOPIC')
     notify_topic_name = os.environ.get('PUBSUB_NOTIFY_TOPIC')
     bucket_in = os.environ.get('BUCKET_INPUT')
     bucket_out = os.environ.get('BUCKET_OUTPUT')
+    publish_script = os.environ.get('PUBLISH_SCRIPT')
 
-    print(f"project_id: {project_id} \nsubscription_id: {subscription_id}\npublish_topic_name: {publish_topic_name}\nBucket in : {bucket_in}\nBucket out : {bucket_out}")
-    receive_messages_with_custom_attributes(project_id, subscription_id, timeout=None)
+    runner = HugoRunner(project_id, subscription_id, notify_topic_name, bucket_in, bucket_out, publish_script)
+
+    logging.info(
+        f"project_id: {project_id} \nsubscription_id: {subscription_id}\npublish_topic_name: {publish_topic_name}\nBucket in : {bucket_in}\nBucket out : {bucket_out}")
+    
+    runner.receive_publish_request()
+    receive_publish_request(
+        project_id, subscription_id, timeout=None)
--- python-app.conf.tpl
+++ python-app.conf.tpl
@@ -1,12 +1,21 @@
 [program:pythonapp]
 directory=#approot#
-command=#approot#/env/bin/honcho start -f ./procfile pubsub
+command=python3 main.py
 autostart=true
 autorestart=true
 user=#user#
+startretries=5
 # Environment variables ensure that the application runs inside of the
 # configured virtualenv.
-environment=VIRTUAL_ENV="#venv#",PATH="#path#",HOME="#home#",USER="#user#",PUBSUB_PROJECT="#PUBSUB_PROJECT#",PUBSUB_PUBLISH_SUBSCRIPTION="#PUBSUB_PUBLISH_SUBSCRIPTION#",PUBSUB_PUBLISH_TOPIC="#PUBSUB_PUBLISH_TOPIC#",PUBSUB_NOTIFY_TOPIC="#PUBSUB_NOTIFY_TOPIC#"
-stdout_logfile=syslog
-stderr_logfile=syslog
+environment=VIRTUAL_ENV="#venv#",PATH="#path#",HOME="#home#",USER="#user#",PUBSUB_PROJECT="#PUBSUB_PROJECT#",PUBSUB_PUBLISH_SUBSCRIPTION="#PUBSUB_PUBLISH_SUBSCRIPTION#",PUBSUB_PUBLISH_TOPIC="#PUBSUB_PUBLISH_TOPIC#",PUBSUB_NOTIFY_TOPIC="#PUBSUB_NOTIFY_TOPIC#",BUCKET_INPUT="#BUCKET_INPUT#",BUCKET_OUTPUT="#BUCKET_OUTPUT#",PUBLISH_SCRIPT="#PUBLISH_SCRIPT#"
+stdout_logfile=#approot#/logs/pubsublogs
+stdout_logfile_maxbytes=1MB
+stdout_logfile_backups=10
+stdout_capture_maxbytes=1MB
+stdout_events_enabled=false
+stderr_logfile=#approot#/logs/pubsublogs
+stderr_logfile_maxbytes=1MB
+stderr_logfile_backups=10
+stderr_capture_maxbytes=1MB
+stderr_events_enabled=false
  
