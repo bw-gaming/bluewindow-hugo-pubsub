7ccfbca550a85e6901e0d2e09121eb946a3c72b7
Aldric Gaudinot <gaudinot@gmail.com>
2021-03-05 18:30:20 +0100

    add timestamp to payload

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import datetime
 import json
 import logging
 import os
 import subprocess
-from concurrent.futures import TimeoutError
 
+from concurrent.futures import TimeoutError
 import google.cloud.logging
 from google.cloud import pubsub_v1
 
@@ -13,22 +14,24 @@ class HugoRunner:
         self.project_id = project_id
         self.subscription_id = subscription_id
         self.notification_topic = notification_topic
-        self.bucket_in  = bucket_in
+        self.bucket_in = bucket_in
         self.bucket_out = bucket_out
         self.publish_script = publish_script
 
         self.timeout = timeout
-        
+
         self.publisher = pubsub_v1.PublisherClient()
         self.notification_topic_path = self.publisher.topic_path(project_id, notification_topic)
 
-    
     def notify(self, payload, update):
         logging.info(f"Notify :{payload}, {update}.")
-        
+
         _payload = {**payload, **update}
+
         data = json.dumps(_payload).encode("utf-8")
-        notification = self.publisher.publish(self.notification_topic_path, data)
+
+        notification = self.publisher.publish(
+            self.notification_topic_path, data)
         logging.info(notification.result())
 
     def run_build(self, payload):
@@ -37,8 +40,8 @@ class HugoRunner:
         website = payload['website']
         theme = payload['theme']
         buildId = payload['buildId']
-        output = subprocess.run(["bash", self.publish_script, environment, website, self.bucket_in, self.bucket_out, buildId, theme], capture_output=True, text=True)
-        return { 'returncode': output.returncode, 'stderr' : output.stderr , 'stdout': output.stdout, 'args': output.args } 
+        output = subprocess.run(["bash", self.publish_script, environment, website,self.bucket_in, self.bucket_out, buildId, theme], capture_output=True, text=True)
+        return {'returncode': output.returncode, 'stderr': output.stderr, 'stdout': output.stdout, 'args': output.args}
 
     def callback(self, message):
         logging.info(f"Received {message.data}.")
@@ -46,17 +49,24 @@ class HugoRunner:
         payload = json.loads(message.data)
         message.ack()
 
-        self.notify(payload, { 'status' : 'running'})
+        self.notify(payload, {'status': 'running'})
         result = self.run_build(payload)
-        self.notify(payload, {'output'  : result, 'status' : 'succeded' if result.returncode == 0 else 'failed' })
-        
+
+        timestamp = int(datetime.datetime.now().timestamp() * 1000)
+
+        self.notify(payload, {
+            'output': result,
+            'status': 'succeded' if result.returncode == 0 else 'failed',
+            'endDate': timestamp
+        })
 
     def receive_publish_request(self):
-        """Receives messages from a pull subscription."""
-        # [START pubsub_subscriber_async_pull_custom_attributes]
         subscriber = pubsub_v1.SubscriberClient()
+
         subscription_path = subscriber.subscription_path(self.project_id, self.subscription_id)
+
         streaming_pull_future = subscriber.subscribe(subscription_path, callback=self.callback)
+
         logging.info(f"Listening for messages on {subscription_path}..\n")
         # Wrap subscriber in a 'with' block to automatically call close() when done.
         with subscriber:
@@ -66,7 +76,6 @@ class HugoRunner:
                 streaming_pull_future.result(timeout=self.timeout)
             except TimeoutError:
                 streaming_pull_future.cancel()
-        # [END pubsub_subscriber_async_pull_custom_attributes]
 
 
 if __name__ == "__main__":
@@ -83,11 +92,12 @@ if __name__ == "__main__":
     bucket_out = os.environ.get('BUCKET_OUTPUT')
     publish_script = os.environ.get('PUBLISH_SCRIPT')
 
-    runner = HugoRunner(project_id, subscription_id, notify_topic_name, bucket_in, bucket_out, publish_script)
+    runner = HugoRunner(project_id, subscription_id,
+                        notify_topic_name, bucket_in, bucket_out, publish_script)
 
     logging.info(
         f"project_id: {project_id} \nsubscription_id: {subscription_id}\npublish_topic_name: {publish_topic_name}\nBucket in : {bucket_in}\nBucket out : {bucket_out}")
-    
+
     runner.receive_publish_request()
     receive_publish_request(
         project_id, subscription_id, timeout=None)
